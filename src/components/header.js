92fd62d6846a8ee4aacc0d7e584d3b84de3910e2
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-01 22:30:03 -0700

    Previous commit was working. This adds a better looking nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -5,40 +5,60 @@ import { navigate } from '@reach/router'
 
 import { logout, isLoggedIn } from "../utils/auth"
 import { Auth } from 'aws-amplify'
+import "./styles.css";
+import { getCurrentUser } from '../utils/auth'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap';
+
+const user = getCurrentUser()
 
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: 'white',
       marginBottom: '1.45rem',
     }}
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={styles.headerTitle}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      {
-        isLoggedIn() && (
-          <p
-            onClick={
-              () => Auth.signOut().then(logout(() => navigate('/app/login'))).catch(err => console.log('eror:', err))
+    {/* Primary Navigation bar */}
+    <nav class="navbar navbar-expand-lg navbar-light nav-padding">
+      <a class="navbar-brand" href="/">{siteTitle}</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="/">Home</a>
+          <a class="nav-item nav-link active" href="/app/signup">SignUp</a>
+          <a class="nav-item nav-link" href="/app/login">Login</a>
+          <a class="nav-item nav-link" href="/app/profile">Profile</a>
+          <a class="nav-item nav-link" href="#">Kanban</a>
+          {/* <a class="nav-item nav-link" href="#">Pricing</a> */}
+
+          <a class="nav-item nav-link" href="#">{user.username}</a>
+          <a class="nav-item nav-link disabled" href="#">Pricing</a>
+          {
+            isLoggedIn() && (
+
+              <a class="nav-item nav-link" href="#">{user.username}</a>
+            )
+          }
+          <a className="nav-item">
+            {
+              isLoggedIn() && (
+                <p
+                  onClick={
+                    () => Auth.signOut().then(logout(() => navigate('/app/login'))).catch(err => console.log('eror:', err))
+                  }
+                  style={styles.link}
+                >Sign Out</p>
+              )
             }
-            style={styles.link}
-          >Sign Out</p>
-        )
-      }
-    </div>
+          </a>
+        </div>
+      </div>
+    </nav>
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
   </div>
 )
 
