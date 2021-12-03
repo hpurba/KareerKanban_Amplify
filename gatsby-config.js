fd776712f5d0c9fd6464dd1e6239b07c5e3d2be1
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 14:25:14 -0700

    Remove non plugins from plugins section of gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,13 +30,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-      resolve: "bootstrap",
-      options: {},
-    },
-    {
-      resolve: "@asseinfo/react-kanban",
-      options: {},
-    },
+    // {
+    //   resolve: "bootstrap",
+    //   options: {},
+    // },
+    // {
+    //   resolve: "@asseinfo/react-kanban",
+    //   options: {},
+    // },
   ],
 }
