7a6c6ee4c9b7078007d073d5243fbe1d8921004b
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 10:35:01 -0700

    ran gatsby build locally

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/hikarupurba/Code/KareerKanban_Amplify/src/pages/404.js"))),
+  "component---src-pages-app-js": hot(preferDefault(require("/Users/hikarupurba/Code/KareerKanban_Amplify/src/pages/app.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/hikarupurba/Code/KareerKanban_Amplify/src/pages/index.js")))
+}
+
