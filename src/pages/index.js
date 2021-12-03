638c0d01ea740ce672acd0d0f5c1cb17bc728413
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 11:30:53 -0700

    import only bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ import Layout from '../components/layout'
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
 import Home from '../components/Home/Home'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap';
 // import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 // import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
