b1da8e0b38da0ca1e556ae550b7be3d884419b34
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 15:14:13 -0700

    Removed bootstrap related imports in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,9 +10,9 @@ import Auth from '@aws-amplify/auth'
 import { setUser } from './src/utils/auth'
 
 // https://stackoverflow.com/questions/56493502/how-to-add-bootstrap-js-in-a-gatsby-website
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
-import "@popperjs/core/dist/umd/popper.min.js"
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/js/bootstrap.min.js"
+// import "@popperjs/core/dist/umd/popper.min.js"
 
 // https://www.gatsbyjs.com/plugins/gatsby-plugin-sass/
 // import "./src/index.scss"
