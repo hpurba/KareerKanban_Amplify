5a3da5a4412a74ca15206dacb04a252c7427eef7
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 01:52:34 -0700

    removed some imports that were preventing a good build

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
 import Home from '../components/Home/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 Amplify.configure(config)
 
