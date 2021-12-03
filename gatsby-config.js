7531593f9c3f88ac465ca240d301332f6bf964c0
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 14:08:40 -0700

    resolve only bootstrap in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     {
-      resolve: "react-bootstrap",
+      resolve: "bootstrap",
       options: {},
     },
     {
