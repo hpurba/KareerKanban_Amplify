ae2255b7616d4da114a1073adc3bbdf7b64c6495
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 11:07:09 -0700

    Moved bootstrap import to layout.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ import Layout from '../components/layout'
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
 import Home from '../components/Home/Home'
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 Amplify.configure(config)
 
