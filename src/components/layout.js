e92dddfafd061915af77aa9b47e26b2ae997559f
Hikaru Purba <hikarupurba@Hikarus-MacBook-Pro.local>
2021-12-03 21:43:20 -0700

    Removed static query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,48 +1,61 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
-
+// import { StaticQuery, graphql } from 'gatsby'
+import PropTypes from "prop-types"
 import Header from './header'
 import './layout.css'
+// import Helmet from 'react-helmet'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Layout = ({ children, data }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Kanban' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: 0,
-            maxWidth: '100vw',
-            padding: 0,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+  // <StaticQuery
+  //   query={graphql`
+  //     query SiteTitleQuery {
+  //       site {
+  //         siteMetadata {
+  //           title
+  //         }
+  //       }
+  //     }
+  //   `}
+  //   render={data => (
+  //     <>
+  //       {/* <Helmet
+  //         title={data.site.siteMetadata.title}
+  //         meta={[
+  //           { name: 'description', content: 'Kanban' },
+  //           { name: 'keywords', content: 'sample, something' },
+  //         ]}
+  //       >
+  //         <html lang="en" />
+  //       </Helmet> */}
+
+  //       <Header siteTitle={data.site.siteMetadata.title} />  
+  //       <div
+  //         style={{
+  //           margin: 0,
+  //           maxWidth: '100vw',
+  //           padding: 0,
+  //           paddingTop: 0,
+  //         }}
+  //       >
+  //         {children}
+  //       </div>
+  //     </>
+  //   )}
+  // />
+  <div>
+    <Header siteTitle="KareerKanban" />
+    <div
+      style={{
+        margin: 0,
+        maxWidth: '100vw',
+        padding: 0,
+        paddingTop: 0,
+      }}
+    >
+      {children}
+    </div>
+  </div>
 )
 
 Layout.propTypes = {
@@ -50,3 +63,58 @@ Layout.propTypes = {
 }
 
 export default Layout
+
+
+
+// import React from 'react'
+// import { StaticQuery, graphql } from 'gatsby'
+// import PropTypes from "prop-types"  
+// import Header from './header'
+// import './layout.css'
+// // import Helmet from 'react-helmet'
+// // import 'bootstrap/dist/css/bootstrap.min.css'
+
+// const Layout = ({ children, data }) => (
+//   <StaticQuery
+//     query={graphql`
+//       query SiteTitleQuery {
+//         site {
+//           siteMetadata {
+//             title
+//           }
+//         }
+//       }
+//     `}
+//     render={data => (
+//       <>
+//         {/* <Helmet
+//           title={data.site.siteMetadata.title}
+//           meta={[
+//             { name: 'description', content: 'Kanban' },
+//             { name: 'keywords', content: 'sample, something' },
+//           ]}
+//         >
+//           <html lang="en" />
+//         </Helmet> */}
+
+//         <Header siteTitle={data.site.siteMetadata.title} />  
+//         <div
+//           style={{
+//             margin: 0,
+//             maxWidth: '100vw',
+//             padding: 0,
+//             paddingTop: 0,
+//           }}
+//         >
+//           {children}
+//         </div>
+//       </>
+//     )}
+//   />
+// )
+
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
+
+// export default Layout
